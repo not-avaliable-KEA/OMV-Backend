0c9c291038090a47e44adaae1cd995659e7064f9
Eliot996 <math10t6@stud.kea.dk>
2022-11-22 13:44:36 +0100

    init

---
+++ src/main/java/com/example/omvbackend/OmvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.omvbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmvBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmvBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/omvbackend/OmvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omvbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmvBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
