2068e3a8464e62c6be60667deebc2b992d044125
Violetta87 <athena_mewo87@hotmail.com>
2022-11-23 11:23:47 +0100

    OSH-21 - we made unit test for getAll

--- src/test/java/com/example/omvbackend/user/service/UserServiceTest.java
+++ src/test/java/com/example/omvbackend/user/service/UserServiceTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 //den starter springboot serveren, den tester som det virker i applikationen, og ikke kun i klassen.
@@ -29,12 +32,24 @@ class UserServiceTest {
         assertNotNull(result.getId());
     }
 
+    @Test
+    void getAll() {
+        //arrange
+        userService.create(new User("tutu", "tamtam"));
+        int expectedSize = 2;
 
+        //act
+        List<User> result = userService.getAll();
 
+        //assert
+        assertEquals(expectedSize,result.size());
 
-
-
-
-
+        //checks that all users are unique in the result array
+        ArrayList<User> usersChecked = new ArrayList<>();
+        for (User u : result) {                    //forloop that runs though the result list,
+            assertFalse(usersChecked.contains(u)); //we expect at false, that the new list doesn contain user u
+            usersChecked.add(u);                   //if the statement is false, we add user u - if true the test fails.
+        }
+    }
 
 }
